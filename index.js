bbcc80f124da66be268ffb8aadce76af78f421f6
dwgydushij <jsjcesseochoa22@hotmail.com>
2024-05-10 18:10:38 +0700

    add more features

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const reverseWords = (str) => str.split(" ").reverse().join(" ");
+function generateParenthesis(n) {
+  const result = [];
+  backtrack("", 0, 0);
+  return result;
+  function backtrack(current, open, close) {
+    if (current.length === 2 * n) result.push(current);
+    if (open < n) backtrack(current + "(", open + 1, close);
+    if (close < open) backtrack(current + ")", open, close + 1);
+  }
+}
